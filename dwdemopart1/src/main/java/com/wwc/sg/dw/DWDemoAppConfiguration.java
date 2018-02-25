4443976068fa2cc877e27eb5a909dcd245cabcda
wandatinkers <ianne.celino@gmail.com>
2018-02-25 13:59:56 +0800

    demo projects part 1

---
+++ dwdemopart1/src/main/java/com/wwc/sg/dw/DWDemoAppConfiguration.java
@@ -0,0 +1,44 @@
+package com.wwc.sg.dw;
+
+import io.dropwizard.Configuration;
+
+public class DWDemoAppConfiguration extends Configuration {
+
+    private String application;
+    private String environment;
+    private String version;
+    private String template;
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+}
